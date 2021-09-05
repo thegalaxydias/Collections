599043f63c14ef2b07c1ecb0b1e7dc8c67c0045b
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-05 13:31:39 +0300

    Добавьте файлы проекта.

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           User admin = new User();
+           User user = new User();
+           User customer = new User();
+
+           List<User> users = new List<User> { admin, user, customer};
+
+            Console.WriteLine(users.Count);
+
+            users.Clear();
+            Console.WriteLine(users.Count);
+        }
+    }
+
+
+    public class User
+    {
+        public string name;
+        public int age;
+        public User() { name = "Unknown"; age = 18; }
+    }
+}
